fdbea87db4fd7becbe078af7f39d012e6d2e7f38
Riya Dwivedi <renudwivedi2004@gmail.com>
2025-05-01 08:44:42 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -3,108 +3,78 @@ import java.util.Scanner;
 
 public class Account {
 
-    private int customerNumber;
-    private int pinNumber;
-    private double checkingBalance = 0;
-    private double savingBalance = 0;
+    private int userId;
+    private int pinCode;
+    private double checkBalance = 0.0;
+    private double saveBalance = 0.0;
 
-    Scanner input = new Scanner(System.in);
-    DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+    Scanner scanner = new Scanner(System.in);
+    DecimalFormat formatter = new DecimalFormat("'$'###,##0.00");
 
-    public void setCustomerNumber(int customerNumber){
-        this.customerNumber = customerNumber;
+    // Setters and Getters
+    public void setUserId(int id) {
+        this.userId = id;
     }
 
-    public int getCustomerNumber(){
-        return customerNumber;
+    public int getUserId() {
+        return userId;
     }
 
-    public void setPinNumber(int pinNumber){
-        this.pinNumber = pinNumber;
+    public void setPinCode(int pin) {
+        this.pinCode = pin;
     }
 
-    public int getPinNumber(){
-        return pinNumber;
+    public int getPinCode() {
+        return pinCode;
     }
 
-    public double getCheckingBalance() {
-        return checkingBalance;
+    public double getCheckBalance() {
+        return checkBalance;
     }
 
-    public double getSavingBalance(){
-        return savingBalance;
+    public double getSaveBalance() {
+        return saveBalance;
     }
 
-    public void calcCheckingWithdraw(double amount){
-        checkingBalance = (checkingBalance - amount);
+    // Balance Operations
+    public void withdrawFromChecking(double amount) {
+        checkBalance -= amount;
     }
 
-    public void calcSavingWithdraw(double amount){
-        savingBalance = (savingBalance - amount);
+    public void withdrawFromSaving(double amount) {
+        saveBalance -= amount;
     }
 
-    public void calcCheckingDeposit(double amount){
-        checkingBalance = (checkingBalance + amount);
+    public void depositToChecking(double amount) {
+        checkBalance += amount;
     }
 
-    public void calcSavingDeposit(double amount){
-        savingBalance = (savingBalance + amount);
+    public void depositToSaving(double amount) {
+        saveBalance += amount;
     }
 
-    public void getCheckingWithdrawInput() {
-        System.out.println("Checking Account balance: " + moneyFormat.format(checkingBalance));
-        System.out.print("Amount you want to withdraw from Checking Account: ");
-        double amount = input.nextDouble();
+    // User Interaction Methods
+    public void processCheckingWithdrawal() {
+        System.out.println("Current Checking Balance: " + formatter.format(checkBalance));
+        System.out.print("Enter amount to withdraw from checking: ");
+        double amount = scanner.nextDouble();
 
-        if(checkingBalance - amount >= 0){
-            calcCheckingWithdraw(amount);
-            System.out.println("New Checking Account Balance: " + moneyFormat.format(checkingBalance));
-        }
-        else{
-            System.out.println("Not Enough Money to Withdraw");
-        }
-    }
-
-    public void getSavingWithdrawInput() {
-        System.out.println("Saving Account balance: " + moneyFormat.format(savingBalance));
-        System.out.print("Amount you want to withdraw from Saving Account: ");
-        double amount = input.nextDouble();
-
-        if(savingBalance - amount >= 0){
-            calcSavingWithdraw(amount);
-            System.out.println("New Saving Account Balance: " + moneyFormat.format(savingBalance));
-        }
-        else{
-            System.out.println("Not Enough Money to Withdraw");
+        if (checkBalance >= amount) {
+            withdrawFromChecking(amount);
+            System.out.println("Updated Checking Balance: " + formatter.format(checkBalance));
+        } else {
+            System.out.println("Insufficient funds in checking account.");
         }
     }
 
-    public void getCheckingDepositInput(){
-        System.out.println("Checking Account Balance: " + moneyFormat.format(checkingBalance));
-        System.out.print("Amount you want to deposit to Checking Account: ");
-        double amount = input.nextDouble();
+    public void processSavingWithdrawal() {
+        System.out.println("Current Saving Balance: " + formatter.format(saveBalance));
+        System.out.print("Enter amount to withdraw from saving: ");
+        double amount = scanner.nextDouble();
 
-        if(checkingBalance + amount >= 0){
-            calcCheckingDeposit(amount);
-            System.out.println("New Checking Account Balance: " + moneyFormat.format(checkingBalance));
-        }
-        else{
-            System.out.println("No Money to Deposit");
-        }
-    }
+        if (saveBalance >= amount) {
+            withdrawFromSaving(amount);
+            System.out.println("Updated Saving Balance: " + formatter.format(saveBalance));
+        } else {
+            System.out.println("Insufficient funds in saving account.");
 
-    public void getSavingDepositInput(){
-        System.out.println("Saving Account Balance: " + moneyFormat.format(savingBalance));
-        System.out.print("Amount you want to deposit to Saving Account: ");
-        double amount = input.nextDouble();
-
-        if(checkingBalance + amount >= 0){
-            calcSavingDeposit(amount);
-            System.out.println("New Saving Account Balance: " + moneyFormat.format(savingBalance));
-        }
-        else{
-            System.out.println("No Money to Deposit");
-        }
-    }
-
-}
